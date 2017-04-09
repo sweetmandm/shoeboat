eba4babf9c4e4fb84a8e8c6884f3ba5c328d39a0
David Sweetman <david@davidsweetman.com>
2017-04-09 10:35:27 -0400

    Count bytes as they pass through

--- lib/proxy_delegate.ex
+++ lib/proxy_delegate.ex
@@ -11,47 +11,46 @@ defmodule Shoeboat.ProxyDelegate do
     loop_pid = spawn_link(fn ->
       receive do :ready -> :ok end
       :ok = :inet.setopts(downstream, [:binary, packet: 0, active: true, nodelay: true])
-      proxy_loop(downstream, upstream)
+      proxy_loop(downstream, upstream, 0, 0)
     end)
     {:ok, loop_pid}
   end
 
-  defp proxy_loop(downstream_socket, upstream_socket) do
+  defp proxy_loop(downstream_socket, upstream_socket, amount_down, amount_up) do
     receive do
       {:tcp, socket, data} when socket == downstream_socket ->
         IO.inspect data
-        relay_downstream(upstream_socket, data)
-        proxy_loop(downstream_socket, upstream_socket)
+        {:ok, count} = relay_to(upstream_socket, data)
+        proxy_loop(downstream_socket, upstream_socket, amount_down + count, amount_up)
       {:tcp_error, socket, reason} when socket == downstream_socket ->
         Logger.info "tcp_error downstream #{reason}"
-        relay_downstream(upstream_socket, <<>>)
+        relay_to(upstream_socket, <<>>)
       {:tcp_closed, socket} when socket == downstream_socket ->
         Logger.info "Downstream socket closed"
-        relay_downstream(upstream_socket, <<>>)
+        relay_to(upstream_socket, <<>>)
         :gen_tcp.close(upstream_socket)
+        Logger.info "Total bytes >#{amount_down} <#{amount_up}"
       {:tcp, socket, data} when socket == upstream_socket ->
         IO.inspect data
-        relay_upstream(downstream_socket, data)
-        proxy_loop(downstream_socket, upstream_socket)
+        {:ok, count} = relay_to(downstream_socket, data)
+        proxy_loop(downstream_socket, upstream_socket, amount_down, amount_up + count)
       {:tcp_error, socket, reason} when socket == upstream_socket ->
         Logger.info "tcp_error upstream #{reason}"
-        relay_upstream(downstream_socket, <<>>)
+        relay_to(downstream_socket, <<>>)
       {:tcp_closed, socket} when socket == upstream_socket ->
         Logger.info "Upstream socket closed"
-        relay_upstream(downstream_socket, <<>>)
+        relay_to(downstream_socket, <<>>)
         :gen_tcp.close(downstream_socket)
+        Logger.info "Total bytes >#{amount_down} <#{amount_up}"
       other ->
         Logger.error "Invalid message:"
         IO.inspect other
-        proxy_loop(downstream_socket, upstream_socket)
+        proxy_loop(downstream_socket, upstream_socket, amount_down, amount_up)
     end
   end
 
-  def relay_upstream(downstream_socket, data) do
-    :gen_tcp.send(downstream_socket, data)
-  end
-
-  def relay_downstream(upstream_socket, data) do
-    :gen_tcp.send(upstream_socket, data)
+  def relay_to(socket, data) do
+    :ok = :gen_tcp.send(socket, data)
+    {:ok, byte_size(data)}
   end
 end
